07074b1b3b84d6d35c5ff97bfbbfc698a4662a34
Bodo Kaiser <bodo.kaiser@physik.uni-muenchen.de>
2023-04-05 10:54:22 +0200

    added api/fluid.md and index.md to docs/src

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,9 @@
 using Documenter, HeatTransferFluids
 
-makedocs()
+makedocs(
+    modules = [HeatTransferFluids],
+    sitename = "HeatTransferFluids.jl",
+    pages = Any["HeatTransferFluids.jl"=>"index.md", "API references"=>Any["api/fluid.md"]],
+)
 
 deploydocs(repo = "github.com/ryd-yb/HeatTransferFluids.jl.git")
---
+++ docs/src/api/fluid.md
@@ -0,0 +1,5 @@
+# Fluid
+
+```@docs
+HeatTransferFluids.Fluid
+```
