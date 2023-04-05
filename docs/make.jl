fe38125900d9c24ffe7fd62191a069de29fe4ad8
Bodo Kaiser <bodo.kaiser@physik.uni-muenchen.de>
2023-04-05 12:57:10 +0200

    added missing api references docs to docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,16 @@ using Documenter, HeatTransferFluids
 makedocs(
     modules = [HeatTransferFluids],
     sitename = "HeatTransferFluids.jl",
-    pages = Any["HeatTransferFluids.jl"=>"index.md", "API references"=>Any["api/fluid.md"]],
+    pages = Any[
+        "HeatTransferFluids.jl"=>"index.md",
+        "API references"=>Any[
+            "api/fluid.md",
+            "api/structures.md",
+            "api/flow.md",
+            "api/pressure_drop.md",
+            "api/heat_transfer.md",
+        ],
+    ],
 )
 
 deploydocs(repo = "github.com/ryd-yb/HeatTransferFluids.jl.git")
